f0ea778aa932c6df385771701bc9063280c4add9
Haochang Qian <fakedestinyck@gmail.com>
2017-11-02 19:41:39 +0000

    add scoreboard functions

--- hw1.js
+++ hw1.js
@@ -3,6 +3,17 @@
  
 
     $.fn.hexed = function( options ) {
+        
+        var currentRound = 1;
+        var totalScore = 0;
+
+        var red;
+        var green;
+        var blue;
+
+        var gotitButton = this.find('#startButton');
+        var totalScoreLabel = this.find('#score h3:nth-child(1)');
+        var currentScoreLabel = this.find('#score h3:nth-child(2)');
  
         var settings = $.extend({
             // These are the defaults.
@@ -10,16 +21,20 @@
             turns: 10
         }, options );
  
-        var red = "00".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
-        var blue = "00".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
-        var green = "00".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
-
-        var color =  '#' + red + blue + green;
-
-        var targetColor = document.getElementById("targetColorImage");
-        var targetFiller = targetColor.getContext("2d");
-        targetFiller.fillStyle = color;
-        targetFiller.fillRect(0,0,100,100);
+        var createNewColor = function() {
+            // this function is written by Osama
+            red = "00".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+            blue = "00".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+            green = "00".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+    
+            var color =  '#' + red + blue + green;
+    
+            var targetColor = document.getElementById("targetColorImage");
+            var targetFiller = targetColor.getContext("2d");
+            targetFiller.fillStyle = color;
+            targetFiller.fillRect(0,0,100,100);
+        };
+        
         
         var myUpdate = function(){
 
@@ -44,6 +59,8 @@
             myFiller.fillStyle = userColor;
             myFiller.fillRect(0,0,100,100);
         };
+        
+        createNewColor();
 
         var redSlider = document.getElementById("redSlider");
         var redOutput = document.getElementById("redValue");
@@ -88,6 +105,58 @@
             myUpdate();
         };
         // this.html("difficulty: " + settings.difficulty + " random color: " + color);
+        
+        var calcScore = function () {
+            var percentOffRed = Math.abs(parseInt(red,16)-redSlider.value)/255*100;
+            var percentOffGreen = Math.abs(parseInt(green,16)-greenSlider.value)/255*100;
+            var percentOffBlue = Math.abs(parseInt(blue,16)-blueSlider.value)/255*100;
+            var percentOff = (percentOffRed+percentOffGreen+percentOffBlue)/3;
+            // alert("red"+parseInt(red,16));
+            // alert(redSlider.value);
+            // alert(percentOffRed);
+            // alert("green"+parseInt(green,16));
+            // alert(greenSlider.value);
+            // alert(percentOffGreen);
+            // alert("blue"+parseInt(blue,16));
+            // alert(blueSlider.value);
+            // alert(percentOffBlue);
+            var timeTaken = 0;
+            var rawScore = ((15-settings.difficulty-percentOff)/(15-settings.difficulty)) * (15000-timeTaken);
+            // alert(15-settings.difficulty-percentOff);
+            if (rawScore < 0) {
+                rawScore = 0;
+            }
+            var alertString = "percent off: " + percentOff + "\n";
+            alertString += "milliseconds taken: " + timeTaken + "\n";
+            alertString += "score: " + rawScore;
+            alert(alertString);
+            return Math.round(rawScore*100)/100;
+        };
+        
+        
+        // implement gotIt! Button
+
+        gotitButton.click(function () {
+           // if the timer isn't start, make an alert
+            if ( 1 === 0) {
+                window.alert("Please start the game first");
+            } else {
+                // TODO: stop timer
+                var thisScore = calcScore();
+                currentScoreLabel.text("Score on Last Color: "+thisScore);
+                totalScore += thisScore;
+                totalScoreLabel.text("Your Score: "+totalScore);
+                if (currentRound === settings.turns) {
+                    alert("Game over!\nYour total score: "+ totalScore);
+                } else {
+                    // TODO: restart timer
+                    currentRound += 1;
+                    createNewColor();
+                }
+            }
+        });
+        
+        
         return this;
  
     };
@@ -96,21 +165,9 @@
 
 
 $(document).ready(function() {
-    
-    var totalRounds;
-    var currentRound = 1;
-    var totalScore;
 
  	$("#hexGame").hexed();
 
-
-
-    $("#gotitButton").click(function(){
-        //if the timer isn't start
-        alert("You have to start the game first!");
-        
-        
-    });
 });
 
 /* When the user clicks on the button, 
